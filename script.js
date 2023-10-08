a2ef7ebc65395f5811052b7e660e036965240691
David <alumnodavidfei@gmail.com>
2023-10-08 11:05:50 +0200

    commit 4

--- script.js
+++ script.js
@@ -1,65 +1,94 @@
 $(document).ready(function () {
-    
-  for (var k = 1; k < faceNumbers; k++) { //crea las img en los div
-
-    var imgA = $("<img>", { src: "img/1.PNG",  width: "45", height: "45", id: "a" + k });
-    var imgB = $("<img>", {  src: "img/2.PNG",  width: "45", height: "45", id: "b" + k });
-
-      $("#" + k).append(imgA); //mete las img
-      $("#" + k).append(imgB);
-
-      $("#a" + k).hide();
-      $("#b" + k).hide();//oculta las img
-  }
-
+  $("#boton").prop("disabled", true);
+  $("#boton2").prop("disabled", true);
+  
 });
 
+var faceNumbers;
 
-    var faceNumbers = 10;
-    var theArray = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-    var arrayDesordenado = desordenarArray(theArray); //randomizar contenido array
-    var index = 0;
-    var bucleInterval, reloadInterval;
-    var tiempoClick;
+var theArray = [];
+var arrayDesordenado; //randomizar contenido array
+var index = 0;
+var bucleInterval, reloadInterval;
+var tiempoClick;
 
-function initGame(varNivelSeleccionado) {
 
-      tiempoClick = parseInt(varNivelSeleccionado); //el tiempo que en que va desaparecer la img
+function initContent(varFaceNumber){
+ 
+  faceNumbers = varFaceNumber;
 
-      $("#boton").prop("disabled", true);
-      $("#boton2").prop("disabled", true);
+  for (var k = 1; k < faceNumbers; k++) {
+    //crea las img en los div
 
-      bucleInterval = setInterval(functionInterval, 20); //llamadas asincronas a la funcion
-      desvanece(index);
+    
+    var automaticDiv = $("<div/>", { id: k, onclick: "functionGetId($(this).attr('id'))", text: k });
+
+    $(".flex-container").append(automaticDiv);
+
+    var imgA = $("<img>", { src: "img/1.PNG", width: "45",height: "45", id: "a" + k});
+    var imgB = $("<img>", {src: "img/2.PNG",width: "45",height: "45", id: "b" + k});
+
+    $("#" + k).append(imgA); //mete las img
+    $("#" + k).append(imgB);
+
+    $("#a" + k).hide();
+    $("#b" + k).hide(); //oculta las img
+
+    theArray[k] = k;
+  }
+
+
+  $("#boton").prop("disabled", false);
+  $("#boton2").prop("disabled", false);
+  $("#boton3").prop("disabled", true);
+
+arrayDesordenado = desordenarArray(theArray);
 }
 
-function functionGetId(varId) { //cada vez que se hace click
-  if (varId == arrayDesordenado[index]) { // el id donde se hace click coincide con el id del div
+function initGame(varNivelSeleccionado) {
+  tiempoClick = parseInt(varNivelSeleccionado); //el tiempo que en que va desaparecer la img
 
-      $("#" + varId).stop(); //para el fadeTo
-      jQuery("#" + varId).css("opacity", "1"); //devuelve la opacidad si no no se veria el careto
-      $("#a" + varId).show(); //muestra careto sonriente
+  $("#boton").prop("disabled", true);
+  $("#boton2").prop("disabled", true);
+ // $("#boton3").prop("disabled", true);
+  bucleInterval = setInterval(functionInterval, 20); //llamadas asincronas a la funcion
+  desvanece(index);
+}
 
-      desvanece(index++);
+function functionGetId(varId) {
+  //cada vez que se hace click
+  if (varId == arrayDesordenado[index]) {
+    // el id donde se hace click coincide con el id del div
+
+    $("#" + varId).stop(); //para el fadeTo
+    jQuery("#" + varId).css("opacity", "1"); //devuelve la opacidad si no no se veria el careto
+    $("#a" + varId).show(); //muestra careto sonriente
+
+    desvanece(index++);
   }
 }
 
 function desvanece() {
-      $("#" + arrayDesordenado[index]).fadeTo(tiempoClick, 0.0);
+  $("#" + arrayDesordenado[index]).fadeTo(tiempoClick, 0.0);
 
-      tiempoClick = tiempoClick * 0.9; 
+  tiempoClick = tiempoClick * 0.9;
+
+  console.log("el index: " + index);
 }
 
-function functionInterval() { //funcion asincrona, comprueba si algun div a llegado a opacidad 0
+function functionInterval() {
+  //funcion asincrona, comprueba si algun div a llegado a opacidad 0
   for (var h = 1; h < faceNumbers; h++) {
-    if ($("#" + h).css("opacity") == 0) { //no ha sido capaz de hacer click
+    if ($("#" + h).css("opacity") == 0) {
+      //no ha sido capaz de hacer click
       jQuery("#" + h).css("opacity", "1");
       $("#b" + h).show(); //muestra careto de rabia
       desvanece(index++);
     }
   }
 
-  if (index == 9) { //fin del juego
+  if (index == faceNumbers - 1) {
+    //fin del juego
     clearInterval(bucleInterval); //para el bucle de intervalo
     reloadInterval = setInterval(reloadGame, 2000); //dar un tiempo antes de hacer el reload
   }
@@ -70,6 +99,6 @@ function desordenarArray(array) {
 }
 
 function reloadGame() {
-    clearInterval(reloadInterval);
-    location.reload();
+  clearInterval(reloadInterval);
+  location.reload();
 }

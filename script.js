2b19c3f6622a4168c6e2cf125e4d98ee11c1a62c
David <alumnodavidfei@gmail.com>
2023-10-09 16:40:32 +0200

    commit 4

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
   $("#boton").prop("disabled", true);
   $("#boton2").prop("disabled", true);
-  
+  $(".progress").hide(); 
 });
 
 var faceNumbers;
@@ -9,48 +9,78 @@ var faceNumbers;
 var theArray = [];
 var arrayDesordenado; //randomizar contenido array
 var index = 0;
-var bucleInterval, reloadInterval;
+var bucleInterval,  tooltipInterval, tooltipIntervalFail;
 var tiempoClick;
+var counterFailures=0;
 
 
-function initContent(varFaceNumber){
- 
+function initContent(varFaceNumber) {
   faceNumbers = varFaceNumber;
 
   for (var k = 1; k < faceNumbers; k++) {
     //crea las img en los div
 
+    var automaticDiv = $("<div/>", {
+      id: k,
+      onclick: "functionGetId($(this).attr('id'))",
+      class:"rounded-circle shadow-sm",
+      text: k,
+    });
+
+    automaticDiv.attr("data-toggle", "tooltip");
+    automaticDiv.attr("data-placement", "top");
+    automaticDiv.attr("title", "OK!");
+
     
-    var automaticDiv = $("<div/>", { id: k, onclick: "functionGetId($(this).attr('id'))", text: k });
+
+    $("#" + k).tooltip("hide");
 
     $(".flex-container").append(automaticDiv);
 
-    var imgA = $("<img>", { src: "img/1.PNG", width: "45",height: "45", id: "a" + k});
-    var imgB = $("<img>", {src: "img/2.PNG",width: "45",height: "45", id: "b" + k});
+    var imgA = $("<img>", {
+      src: "img/1.PNG",
+      width: "45",
+      height: "45",
+      class:"rounded-circle",
+      id: "a" + k
+    });
+    var imgB = $("<img>", {
+      src: "img/2.PNG",
+      width: "45",
+      height: "45",
+      class:"rounded-circle",
+      id: "b" + k
+    });
+
+    imgB.attr("data-toggle", "tooltip");
+    imgB.attr("data-placement", "top");
+    imgB.attr("title", "NO!");
+    $("#b" + k).tooltip("hide");
 
     $("#" + k).append(imgA); //mete las img
     $("#" + k).append(imgB);
 
-    $("#a" + k).hide();
+    $("#a" + k).hide();   //class="bg-primary"
     $("#b" + k).hide(); //oculta las img
 
     theArray[k] = k;
   }
 
-
   $("#boton").prop("disabled", false);
   $("#boton2").prop("disabled", false);
   $("#boton3").prop("disabled", true);
 
-arrayDesordenado = desordenarArray(theArray);
+  arrayDesordenado = desordenarArray(theArray);
 }
 
 function initGame(varNivelSeleccionado) {
+
+  $('#myModal').modal('hide');
   tiempoClick = parseInt(varNivelSeleccionado); //el tiempo que en que va desaparecer la img
 
   $("#boton").prop("disabled", true);
   $("#boton2").prop("disabled", true);
- // $("#boton3").prop("disabled", true);
+
   bucleInterval = setInterval(functionInterval, 20); //llamadas asincronas a la funcion
   desvanece(index);
 }
@@ -58,8 +88,9 @@ function initGame(varNivelSeleccionado) {
 function functionGetId(varId) {
   //cada vez que se hace click
   if (varId == arrayDesordenado[index]) {
+    $("#" + varId).tooltip("show");
     // el id donde se hace click coincide con el id del div
-
+    tooltipInterval = setInterval(functionStopTooltip, 500, "a", varId);
     $("#" + varId).stop(); //para el fadeTo
     jQuery("#" + varId).css("opacity", "1"); //devuelve la opacidad si no no se veria el careto
     $("#a" + varId).show(); //muestra careto sonriente
@@ -68,12 +99,28 @@ function functionGetId(varId) {
   }
 }
 
+function functionStopTooltip(a, b) {
+  if (a == "a") {
+    $("#" + b).tooltip("hide");
+    $("#" + b).addClass("bg-success");
+
+    clearInterval(tooltipInterval);
+  }
+
+  if (a == "b") {
+    $("#b" + b).tooltip("hide");
+ 
+    clearInterval(tooltipIntervalFail);
+  }
+}
+
+
 function desvanece() {
-  $("#" + arrayDesordenado[index]).fadeTo(tiempoClick, 0.0);
+     $("#" + arrayDesordenado[index]).fadeTo(tiempoClick, 0.0);
 
-  tiempoClick = tiempoClick * 0.9;
+     tiempoClick = tiempoClick * 0.9;
 
-  console.log("el index: " + index);
+  
 }
 
 function functionInterval() {
@@ -83,22 +130,56 @@ function functionInterval() {
       //no ha sido capaz de hacer click
       jQuery("#" + h).css("opacity", "1");
       $("#b" + h).show(); //muestra careto de rabia
+      $("#b" + h).tooltip("show");
+      
+      counterFailures++;
+      
+      $("#" + h).addClass("bg-warning");
+      tooltipInterval = setInterval(functionStopTooltip, 200, "b", h);
+
       desvanece(index++);
     }
   }
 
-  if (index == faceNumbers - 1) {
-    //fin del juego
-    clearInterval(bucleInterval); //para el bucle de intervalo
-    reloadInterval = setInterval(reloadGame, 2000); //dar un tiempo antes de hacer el reload
-  }
+  if (index == faceNumbers - 1) barra();
+
 }
+
+function barra(){
+  
+  $(".progress").show(); 
+
+  var percentGame =1 - (counterFailures / (faceNumbers - 1));
+  percentGame = percentGame * 100;
+  
+  var automaticDiv = $("<div/>", {
+    class: "progress-bar bg-success",
+    style: "width:"+percentGame+"%",
+    text : (percentGame)+"%"
+   });
+
+
+  
+  $(".progress").append(automaticDiv);
+  
+   automaticDiv = $("<div/>", {
+    class: "progress-bar bg-warning",
+    style: "width:"+(100-percentGame)+"%",
+    text : (100-percentGame)+"%"
+ 
+   });
+
+   $(".progress").append(automaticDiv);
+  clearInterval(bucleInterval); //para el bucle de intervalo
+
+}
+
+
 
 function desordenarArray(array) {
   return array.sort(() => Math.random() - 0.5); //desordena array, proceso estocastico !
 }
 
-function reloadGame() {
-  clearInterval(reloadInterval);
+function newGame(){
   location.reload();
 }
